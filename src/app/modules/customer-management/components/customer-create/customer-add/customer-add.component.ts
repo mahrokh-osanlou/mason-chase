726eb9c27f3a9cba3e10045eb6770cb3a3d96d8f
mahrokh osanlou <mahrokh.osanlou.dev@gmail.com>
2023-04-14 19:28:41 +0330

    customer create done

--- src/app/modules/customer-management/components/customer-create/customer-add/customer-add.component.ts
+++ src/app/modules/customer-management/components/customer-create/customer-add/customer-add.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CustomerManagementService } from "../../../services/customer.service";
+import { MyErrorStateMatcher } from "src/app/modules/shared/classes/error-state-matcher";
+import { debounceTime } from "rxjs";
+import { ProgressSpinnerMode } from "@angular/material/progress-spinner";
 
 @Component({
-  selector: 'app-customer-add',
-  templateUrl: './customer-add.component.html',
-  styleUrls: ['./customer-add.component.scss']
+  selector: "app-customer-add",
+  templateUrl: "./customer-add.component.html",
+  styleUrls: ["./customer-add.component.scss"],
 })
 export class CustomerAddComponent implements OnInit {
+  matcher = new MyErrorStateMatcher();
+  maxDate = new Date();
+  isLoading: boolean = false;
+  mode: ProgressSpinnerMode = "indeterminate";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public customerManagementService: CustomerManagementService) {
+    if (!this.customerManagementService.editMode) {
+      this.customerManagementService.emailFormControl.valueChanges
+        .pipe(debounceTime(1000))
+        .subscribe((term) => {
+          this.customerManagementService.exist = null;
+          if (term && this.customerManagementService.emailFormControl.valid) {
+            const exist = this.customerManagementService.emails.has(
+              term.toLowerCase()
+            );
+            this.customerManagementService.exist = exist;
+          }
+        });
+    }
   }
 
+  ngOnInit(): void {}
 }
--- src/app/modules/customer-management/components/customer-create/customer-create.component.ts
+++ src/app/modules/customer-management/components/customer-create/customer-create.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { CustomerManagementService } from "../../services/customer.service";
 
 @Component({
-  selector: 'app-customer-create',
-  templateUrl: './customer-create.component.html',
-  styleUrls: ['./customer-create.component.scss']
+  selector: "app-customer-create",
+  templateUrl: "./customer-create.component.html",
+  styleUrls: ["./customer-create.component.scss"],
 })
-export class CustomerCreateComponent implements OnInit {
-
-  constructor() { }
+export class CustomerCreateComponent implements OnInit, OnDestroy {
+  constructor(public customerManagementService: CustomerManagementService) {}
 
   ngOnInit(): void {
+    this.customerManagementService.selectedIndex = 0;
+    this.customerManagementService.customerList();
   }
 
+  setIndex(event: any) {
+    this.customerManagementService.selectedIndex = event.index;
+  }
+
+  ngOnDestroy(): void {
+    this.customerManagementService.firstNameFormControl.reset();
+    this.customerManagementService.lastNameFormControl.reset();
+    this.customerManagementService.emailFormControl.reset();
+    this.customerManagementService.phoneFormControl.reset();
+    this.customerManagementService.birthDateFormControl.reset();
+    this.customerManagementService.bankAccountNumberFormControl.reset();
+    this.customerManagementService.success = false;
+    this.customerManagementService.result = false;
+    this.customerManagementService.editMode = false;
+  }
 }
