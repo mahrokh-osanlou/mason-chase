59b1e419427550de98efc920160dd68051d17bb8
mahrokh osanlou <mahrokh.osanlou.dev@gmail.com>
2023-04-14 12:14:00 +0330

    home routing edited

--- src/app/core/home/home-routing.module.ts
+++ src/app/core/home/home-routing.module.ts
@@ -1,12 +1,21 @@
 import { Routes } from "@angular/router";
 import { AuthGuard } from "src/app/core/guards/auth.guard";
 import { HomeComponent } from "./components/home/home.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 export const routes: Routes = [
   {
     path: "",
-    loadChildren: () =>
-      import("../../modules/customer-management/customer-management.module").then((m) => m.CustomerManagementModule),
+    component: HomeComponent,
+    children: [
+      {
+        path: "",
+        loadChildren: () =>
+          import(
+            "../../modules/customer-management/customer-management.module"
+          ).then((m) => m.CustomerManagementModule),
+      },
+    ],
   },
 ];
-export const routedComponents = [HomeComponent];
+export const routedComponents = [HomeComponent, NavbarComponent];
