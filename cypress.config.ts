25246ec2b791c95ffcef792b5356b3689a3a1789
mahrokh osanlou <mahrokh.osanlou.dev@gmail.com>
2023-06-07 16:33:43 +0330

    cypress config updated

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,34 @@
-import { defineConfig } from 'cypress'
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
+import * as createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  
-  e2e: {
-    'baseUrl': 'http://localhost:4200',
-    supportFile: false
-  },
-  
-  
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
-    specPattern: '**/*.cy.ts'
-  }
-  
-})
+    specPattern: '**/*.cy.ts',
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    specPattern: '**/*.cy.ts',
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ): Promise<Cypress.PluginConfigOptions> {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
+    },
+  },
+});
