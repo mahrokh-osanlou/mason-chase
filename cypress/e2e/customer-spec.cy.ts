f08d605132f52231d92b9cd6e5bd11f330c4a201
mahrokh osanlou <mahrokh.osanlou.dev@gmail.com>
2023-06-07 16:38:58 +0330

    douplicate email added

--- cypress/e2e/customer-spec.cy.ts
+++ cypress/e2e/customer-spec.cy.ts
@@ -327,4 +327,64 @@ describe("template spec", () => {
         ).to.not.be.true;
       });
   });
+
+  it("douplicateEmail", () => {
+    cy.visit("http://localhost:4200/pages/customer-create");
+
+    cy.get("#firstName")
+      .type("mahrokh")
+      .then((el) => {
+        let input_value = el.val();
+        expect(input_value).to.eq("mahrokh");
+      });
+
+    cy.get("#lastName")
+      .type("osanlou")
+      .then((el) => {
+        let input_value = el.val();
+        expect(input_value).to.eq("osanlou");
+      });
+
+    cy.get("#email")
+      .type("mahrokh@gmail.com")
+      .then((el) => {
+        let input_value = el.val();
+        expect(input_value).to.eq("mahrokh@gmail.com");
+      });
+
+    cy.get("#birthDate")
+      .type("10/27/1991")
+      .then((el) => {
+        let input_value = el.val();
+        expect(input_value).to.eq("10/27/1991");
+      });
+
+    cy.get("#phoneNumber")
+      .type("+989127430071")
+      .then((el) => {
+        let input_value = el.val();
+        expect(input_value).to.eq("+989127430071");
+      });
+
+    cy.get("#bankAccount")
+      .type("12345")
+      .then((el) => {
+        let input_value = el.val();
+        expect(input_value).to.eq("12345");
+      });
+
+    cy.get("#submit")
+      .click()
+      .as("customer")
+      .then(() => {
+        let list = JSON.parse(localStorage.getItem("customerList") ?? "");
+        let exist = list.some((el: any) => {
+          return el.Email == "mahrokh@gmail.com";
+        });
+        expect(
+          exist,
+          "Code : 202, Message: Duplicate customer by Email address!"
+        ).to.not.be.true;
+      });
+  });
 });
